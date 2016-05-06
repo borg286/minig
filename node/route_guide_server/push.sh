ee29e7d93fd32d6ba0a62c10af90dde803364acc
Brian Matthews <borg286@gmail.com>
2016-05-05 19:12:03 -0700

    move base node image Dockerfile into seperate base folder. Also add new route_guide code. Not yet working. Pulled from grpc/examples/node/route_guide. surprised how little I had to change

---
+++ node/route_guide_server/push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+bazel build docker-build-push
+TAG=`docker images | awk '$1~/localhost:5000\/route_guide$/{print $2}' | grep \`whoami\` | sort -r | head -n 1`
+sed -i "s/image: localhost:5000\/route_guide.*/image: localhost:5000\/route_guide:$TAG/" route_guide.yaml
+sed -i "s/build: .*/build: $TAG/" route_guide.yaml
+kubectl apply -f route_guide.yaml
